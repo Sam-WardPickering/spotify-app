ca524675e701e647e1be327c74fa32ab59d4013d
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-02-06 18:14:41 +1300

    First commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000/',
+        clientId: 'd90d32a5cc33474fb2bcd647feeb0375',
+        clientSecret: '6f47066141404cd680f4c86549a9d4b0',
+        refreshToken,
+    })
+
+    spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+        res.json({
+            accessToken: data.body.accessToken,
+            expiresIn: data.body.expiresIn
+        })
+    })
+    .catch(() => {
+        res.sendStatus(400)
+    })
+})
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000/',
+        clientId: 'd90d32a5cc33474fb2bcd647feeb0375',
+        clientSecret: '6f47066141404cd680f4c86549a9d4b0'
+    })
+    spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    })
+    .catch(err => {
+        // console.log(err)
+        res.sendStatus(400)
+    })
+})
+
+app.listen(3001)
